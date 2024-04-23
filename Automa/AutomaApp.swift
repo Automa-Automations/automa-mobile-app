aee233881c615fc9bb70590cc04cc8d40d3bac44
Simon Ferns <simonferns@Simons-iMac-Pro.local>
2024-04-23 07:42:35 +0200

    Initial Setup

---
+++ Automa/AutomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomaApp.swift
+//  Automa
+//
+//  Created by Simon Ferns on 2024/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AutomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
