8062e48c8f4d57d249d7666daf4e75c9c6edfe92
Simon Ferns <business@simonferns.com>
2024-05-07 15:36:49 +0200

    [automa|payments|intial-functional-payments] Payments are functional but not optimal, currently we don't handle any edge cases + We give the users their plan before they pay (Which is a big no-no)

---
+++ Automa/Models/Providers/StripePaymentProvider.swift
@@ -0,0 +1,18 @@
+//
+//  StripePaymentProvider.swift
+//  Automa
+//
+//  Created by Simon Ferns on 2024/05/07.
+//
+
+import SwiftUI
+
+struct StripePaymentProvider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StripePaymentProvider()
+}
