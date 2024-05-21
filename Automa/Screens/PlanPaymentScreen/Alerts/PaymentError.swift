800ca7fb8cb9cd6faffd31f563e3fbc34d993e89
Simon Ferns <business@simonferns.com>
2024-05-21 17:37:35 +0200

    .

--- Automa/Screens/PlanPaymentScreen/Alerts/PaymentError.swift
+++ Automa/Screens/PlanPaymentScreen/Alerts/PaymentError.swift
@@ -4,5 +4,13 @@
 //
 //  Created by Simon Ferns on 2024/05/21.
 //
-
 import Foundation
+import SwiftUI
+
+func PaymentError(alertMessage: String) -> Alert {
+    Alert(
+        title: Text(alertMessage),
+        dismissButton: .default(Text("OK"))
+    )
+}
+
